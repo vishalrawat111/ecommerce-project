3800268fc6d4c4c11b9a46653f3f7954d52b34ab
vishalrawat111 <vishalrawat0458@gmail.com>
2026-03-07 23:11:00 +0530

    fixed route path

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const productRoutes = require("./routes/productRoutes");
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+
+const productRoutes = require("../routes/productRoutes");
 
 dotenv.config();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ MongoDB Connection
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected Successfully"))
   .catch((err) => console.log("MongoDB Connection Error:", err));
@@ -20,8 +21,10 @@ app.get("/", (req, res) => {
   res.send("Ecommerce API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
 app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
